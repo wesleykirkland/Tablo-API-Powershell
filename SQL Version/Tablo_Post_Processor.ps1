b9d755d64f9b67b8332a24b5cd21637b659d7959
Wesley Kirkland <wesley@wesleyk.me>
2016-11-04 17:55:25 -0400

    Fixed Test-SQLConnection
    Err intellisense screwed me over, fixed the variable name

--- SQL Version/Tablo_Post_Processor.ps1
+++ SQL Version/Tablo_Post_Processor.ps1
@@ -26,21 +26,45 @@ $Database = "Tablo"
 
 #Functions
 #Test SQL Server Connection
-Function Test-SQLConnection ($Server) {
-    $connectionString = "Data Source=$Server;Integrated Security=true;Initial Catalog=master;Connect Timeout=3;"
-    $sqlConn = New-Object ("Data.SqlClient.SqlConnection") $connectionString
-    trap
-    {
-        Write-Error "Cannot connect to $Server.";
-        exit
-    }
+Function Test-SQLConnection {
+    #Params
+    [CmdletBinding()]
+    Param(
+    [parameter(Position=0,Mandatory=$true)]
+        $ServerInstance,
+    [parameter()]
+        [System.Management.Automation.PSCredential]$Cred,
+    [parameter()]
+        [switch]$NoSSPI = $false
+    )
 
-    $sqlConn.Open()
-    if ($sqlConn.State -eq 'Open')
-    {
-        $sqlConn.Close();
+    Process {
+        if ($NoSSPI -and (!($Cred))) {
+            $Cred = Get-Credential
+        }
+
+        if ($NoSSPI) {
+            Write-Verbose 'Setting SQL to use local credentials'
+            $connectionString = "Data Source = $ServerInstance;Initial Catalog=master;User ID = $($cred.UserName);Password = $($cred.GetNetworkCredential().password);"
+        } else {
+            Write-Verbose 'Setting SQL to use SSPI'
+            $connectionString = "Data Source=$ServerInstance;Integrated Security=true;Initial Catalog=master;Connect Timeout=3;"
+        }
+
+        $sqlConn = New-Object ("Data.SqlClient.SqlConnection") $connectionString
+        trap {
+            Write-Error "Cannot connect to $Server.";
+            exit
+        }
+
+        $sqlConn.Open()
+        if ($sqlConn.State -eq 'Open') {
+            Write-Verbose "Successfully connected to $ServerInstance"
+            $sqlConn.Close()
+        }
     }
 }
+
 
 #Run-SQLQuery
 Function Run-SQLQuery {
